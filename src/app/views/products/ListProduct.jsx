f0e06da7ab1efa008461d9fe1e4fe8cc63a3ffd3
edwin-cajan <edwin@amauta.tech>
2021-10-03 19:50:56 -0500

    update-view

--- src/app/views/products/ListProduct.jsx
+++ src/app/views/products/ListProduct.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Breadcrumb } from 'app/components';
+import { Breadcrumb, SimpleCard } from 'app/components'
+
 const ListProduct = () => {
   return (
     <div className="m-sm-30">
       <div className="mb-sm-30">
-        <Breadcrumb
-          routeSegments={[
-            { name: 'Listar productos' },
-          ]}
-        />
+        <Breadcrumb routeSegments={[{ name: 'Agregar producto' }]} />
       </div>
+      <SimpleCard title="Simple Table">Listar productos</SimpleCard>
     </div>
   )
 }
