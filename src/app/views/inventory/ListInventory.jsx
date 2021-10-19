e932c06de04a939761aeed0f481ba064fa2f834d
edwin-cajan <edwin@amauta.tech>
2021-10-19 17:32:39 -0500

    .

--- src/app/views/inventory/ListInventory.jsx
+++ src/app/views/inventory/ListInventory.jsx
@@ -27,7 +27,7 @@ const ListInventory = () => {
     setRowsPerPage(+event.target.value)
     setPage(0)
   }
-  const inventarioL=[
+  const inventarioL = [
     {
       SKU: '1231123',
       nombre: 'Lapop Lenovo iCore5',
@@ -47,11 +47,11 @@ const ListInventory = () => {
       marca: 'Ecma',
     },
   ]
-  const serie=[
+  const serie = [
     {
-      nombre:'Lapop Lenovo iCore5',
-      nro:[123123,234234]
-    }
+      nombre: 'Lapop Lenovo iCore5',
+      nro: [123123, 234234],
+    },
   ]
   return (
     <div className="m-sm-30">
@@ -59,7 +59,7 @@ const ListInventory = () => {
         <Breadcrumb routeSegments={[{ name: 'Inventario' }]} />
       </div>
       <SimpleCard title={`${inventarioL.length} productos en el inventario`}>
-      <Table className="whitespace-pre">
+        <Table className="whitespace-pre">
           <TableHead>
             <TableRow>
               <TableCell className="px-0">SKU</TableCell>
@@ -70,67 +70,65 @@ const ListInventory = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {inventarioL.slice(
-              page * rowsPerPage,
-              page * rowsPerPage + rowsPerPage
-            ).map((inv, index) => (
-              <TableRow key={index}>
-                <TableCell className="px-0 capitalize" align="left">
-                  {inv.SKU}
-                </TableCell>
-                <TableCell className="px-0 capitalize" align="left">
-                  {inv.nombre}
-                </TableCell>
-                <TableCell className="px-0 capitalize" align="left">
-                  {inv.cantidad}
-                </TableCell>
-                <TableCell className="px-0 capitalize" align="left">
-                  {inv.marca}
-                </TableCell>
-                <TableCell className="px-0">
-                  <Tooltip title="Visualizar pedido">
-                    <IconButton size="large">
-                      <Icon color="primary">visibility</Icon>
-                    </IconButton>
-                  </Tooltip>
-                </TableCell>
-              </TableRow>
-            ))}
+            {inventarioL
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((inv, index) => (
+                <TableRow key={index}>
+                  <TableCell className="px-0 capitalize" align="left">
+                    {inv.SKU}
+                  </TableCell>
+                  <TableCell className="px-0 capitalize" align="left">
+                    {inv.nombre}
+                  </TableCell>
+                  <TableCell className="px-0 capitalize" align="left">
+                    {inv.cantidad}
+                  </TableCell>
+                  <TableCell className="px-0 capitalize" align="left">
+                    {inv.marca}
+                  </TableCell>
+                  <TableCell className="px-0">
+                    <Tooltip title="Visualizar pedido">
+                      <IconButton size="large">
+                        <Icon color="primary">visibility</Icon>
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
         <Dialog
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
         >
           <SimpleCard>
-          <DialogTitle>
-          Lapop Lenovo iCore5
-          </DialogTitle>
-          <DialogContent>
-          <Table className="whitespace-pre">
-          <TableHead>
-            <TableRow>
-              <TableCell className="px-0">imagen</TableCell>
-              <TableCell className="px-0">Nombre de Serie</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {serie.map((s,index)=>(
-            <TableRow key={index}>
-            <TableCell className="px-0 capitalize" align="left">
-              <img src='https://omnitechperu.com/data/cache/thum_21256nblen81yh00mwlm.jpg' />
-            </TableCell>
-            <TableCell className="px-0 capitalize" align="left">
-              {s.nro}
-            </TableCell>
-            </TableRow>
-          ))}
-          </TableBody>
-          </Table>
-          </DialogContent>
+            <DialogTitle>Lapop Lenovo iCore5</DialogTitle>
+            <DialogContent>
+              <Table className="whitespace-pre">
+                <TableHead>
+                  <TableRow>
+                    <TableCell className="px-0">imagen</TableCell>
+                    <TableCell className="px-0">Nombre de Serie</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {serie.map((s, index) => (
+                    <TableRow key={index}>
+                      <TableCell className="px-0 capitalize" align="left">
+                        <img
+                          alt={s.nro}
+                          src="https://omnitechperu.com/data/cache/thum_21256nblen81yh00mwlm.jpg"
+                        />
+                      </TableCell>
+                      <TableCell className="px-0 capitalize" align="left">
+                        {s.nro}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </DialogContent>
           </SimpleCard>
-          
-        
         </Dialog>
         <TablePagination
           className="px-4"
