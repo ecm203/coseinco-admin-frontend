1d4b00dc075ce0d9098ca48ae2966fd80eec30cc
edwin-cajan <edwin@amauta.tech>
2021-10-28 22:28:13 -0500

    product list update

--- src/app/views/products/ListProduct.jsx
+++ src/app/views/products/ListProduct.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect}from 'react';
+import React, { useEffect, useState } from 'react'
 import {
   Breadcrumb,
   SimpleCard,
   MaxtBackdrop,
- MatxSnackbar      
-} from 'app/components';
+  MatxSnackbar,
+} from 'app/components'
 import {
   Avatar,
   Button,
@@ -17,40 +17,20 @@ import {
   IconButton,
   Icon,
   Grid,
-} from '@mui/material';
-import axios from 'axios';
-import { useHistory } from 'react-router'
-
-
-const productos = [
-  {
-    SKU: '01112',
-    nombre: 'Prueba 1',
-    precio: "99",
-    estado: 'habilitado',
-    imagen:'https://res.cloudinary.com/dzsitpxzw/image/upload/v1634182049/sample.jpg'
-  },
-  {
-    SKU: '01113',
-    nombre: 'Prueba 2',
-    precio: "199",
-    estado: 'habilitado',
-    imagen:'https://res.cloudinary.com/dzsitpxzw/image/upload/v1634182049/sample.jpg'
-  }
-]
-
-const apiUrl = 'http://localhost:5000/api';
-
+} from '@mui/material'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
+const apiUrl = 'http://localhost:5000/api'
 
 const ListProduct = () => {
   const history = useHistory()
-  const [product, setProduct] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const [product, setProduct] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
   const handleBackTo = () => {
-    //home 
+    //home
   }
-   const handleClose = (event, reason) => {
+  const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return
     }
@@ -58,144 +38,120 @@ const ListProduct = () => {
     setIsError(false)
   }
 
-  useEffect(() =>{
-    setIsLoading(true);
+  useEffect(() => {
+    setIsLoading(true)
     axios.get(`${apiUrl}/productos`).then(
       (response) => {
-        console.log('respose in useEffect: ',response);
+        console.log('respose in useEffect: ', response)
         setProduct(response.data.products)
-        setIsLoading(false);
+        setIsLoading(false)
       },
       (error) => {
-        setIsLoading(false);
+        setIsLoading(false)
         setIsError(true)
       }
-      );
-    },[])
-    
+    )
+  }, [])
+
   return (
-  <>
-    <MaxtBackdrop isOpen={isLoading}/>
-    {
-      !isLoading && !isError && (
-          <div className="m-sm-30">
-            <div className="mb-sm-30">
-              <Breadcrumb
-                routeSegments={[
-                  { name: 'Listar productos' },
-                ]}
-              />
-            </div>
-            <SimpleCard title={'Productos'}>
-                  <div className="overflow-auto">
-                    <Table className={'whitespace-pre min-w-600'}>
-                      <TableHead>
-                        <TableRow>
-                          <TableCell className="px-0" colSpan={1}>
-                            SKU
-                          </TableCell>
-                          <TableCell className="px-0" colSpan={4}>
-                            Producto
-                          </TableCell>
-                          <TableCell className="pr-0" align="right" colSpan={1}>
-                            Precio de venta (s/.)
-                          </TableCell>
-                          <TableCell className="pr-5" align="right" colSpan={1}>
-                            Estado
-                          </TableCell>
-                          <TableCell className="pr-5" align="right" colSpan={1}>
-                            Acciones
-                          </TableCell>
-                          <TableCell className="pr-5" align="right" colSpan={1}>
-                          </TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {product
-                        && product
-                          .map((subscriber, index) => (
-                            <TableRow key={index}>
-                              <TableCell
-                                colSpan={1}
-                                className="px-0 capitalize"
-                                align="left"
-                                >
-                                {subscriber.SKU}
-                              </TableCell>
-                              <TableCell colSpan={4} className="px-0 capitalize" align="left">
-                                <div className="flex items-center">
-                                  {<Avatar src={subscriber.imagenes[0]} />}
-                                  <p className="m-0 ml-4">{subscriber.nombre}</p>
-                                </div>
-                              </TableCell>
-                              <TableCell
-                                  colSpan={1}
-                                  className="pr-5"
-                                  align="right"
-                              >
-                                {subscriber.precio}
-                              </TableCell>
-                              <TableCell
-                                  colSpan={1}
-                                  className="px-1"
-                                  align="right"
-                              >
-                                {subscriber.estado}
-                              </TableCell>
-                              <TableCell
-                                colSpan={2}
-                                className="px-5"
-                              >
-                                <Tooltip title="Visualizar">
-                                    <IconButton
-                                      size="large"
-                                    >
-                                      <Icon color="primary">visibility</Icon>
-                                    </IconButton>
-                                </Tooltip>
-                                <Tooltip title="Editar">
-                                  <IconButton
-                                    size="large"
-                                  >
-                                    <Icon color="primary">edit</Icon>
-                                  </IconButton>
-                                </Tooltip>
-                                <Tooltip title="Anuladar">
-                                  <IconButton
-                                    size="large"
-                                  >
-                                    <Icon color="primary">do_not_disturb_alt</Icon>
-                                  </IconButton>
-                                </Tooltip>
-                                  
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                      </TableBody>
-                    </Table>
-                  </div>
-                  <Grid
-                    container
-                    spacing={3}
-                    className="pt-sm-24"
-                    justifyContent="flex-end"
-                    >
-                    <Grid item lg={3} md={3} sm={12} xs={12}>
-                      <Button
-                        fullWidth
-                        color="secondary"
-                        variant="outlined"
-                        onClick={handleBackTo}
-                        >
-                        <span className="capitalize">Volver atras</span>
-                      </Button>
-                    </Grid>
-                  </Grid>
-            </SimpleCard>
+    <>
+      <MaxtBackdrop isOpen={isLoading} />
+      {!isLoading && !isError && (
+        <div className="m-sm-30">
+          <div className="mb-sm-30">
+            <Breadcrumb routeSegments={[{ name: 'Listar productos' }]} />
           </div>
-      )
-    }
-    {!isLoading && isError && (
+          <SimpleCard title={'Productos'}>
+            <div className="overflow-auto">
+              <Table className={'whitespace-pre min-w-600'}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell className="px-0" colSpan={1}>
+                      SKU
+                    </TableCell>
+                    <TableCell className="px-0" colSpan={4}>
+                      Producto
+                    </TableCell>
+                    <TableCell className="pr-5" align="right" colSpan={1}>
+                      Precio/V
+                    </TableCell>
+                    <TableCell className="pl-5" align="left" colSpan={1}>
+                      Estado
+                    </TableCell>
+                    <TableCell className="px-5" align="left" colSpan={2}>
+                      Acciones
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {product.map((subscriber, index) => (
+                    <TableRow key={index}>
+                      <TableCell
+                        colSpan={1}
+                        className="px-0 capitalize"
+                        align="left"
+                      >
+                        {subscriber.SKU}
+                      </TableCell>
+                      <TableCell
+                        colSpan={4}
+                        className="px-0 capitalize"
+                        align="left"
+                      >
+                        <div className="flex items-center">
+                          {<Avatar src={subscriber.imagenes[0]} />}
+                          <p className="m-0 ml-4">{subscriber.nombre}</p>
+                        </div>
+                      </TableCell>
+                      <TableCell colSpan={1} className="pr-5" align="right">
+                        {'$. ' + subscriber.precio}
+                      </TableCell>
+                      <TableCell className="pl-5" colSpan={1}>
+                        {subscriber.estado}
+                      </TableCell>
+                      <TableCell colSpan={2} className="px-5">
+                        <Tooltip title="Visualizar">
+                          <IconButton size="large">
+                            <Icon color="primary">visibility</Icon>
+                          </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Editar">
+                          <IconButton size="large">
+                            <Icon color="primary">edit</Icon>
+                          </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Anular">
+                          <IconButton size="large">
+                            <Icon color="primary">do_not_disturb_alt</Icon>
+                          </IconButton>
+                        </Tooltip>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+            <Grid
+              container
+              spacing={3}
+              className="pt-sm-24"
+              justifyContent="flex-end"
+            >
+              <Grid item lg={3} md={3} sm={12} xs={12}>
+                <Button
+                  fullWidth
+                  color="secondary"
+                  variant="outlined"
+                  onClick={handleBackTo}
+                >
+                  Volver
+                </Button>
+              </Grid>
+            </Grid>
+          </SimpleCard>
+        </div>
+      )}
+      {!isLoading && isError && (
         <MatxSnackbar
           open={isError}
           title={'Error'}
