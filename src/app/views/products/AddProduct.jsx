1dd798f1df5a51b74c8358777fadd691c67bf30f
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-11-11 23:15:45 -0500

    drag&drop image

--- src/app/views/products/AddProduct.jsx
+++ src/app/views/products/AddProduct.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Breadcrumb, SimpleCard } from 'app/components'
 import { Button, Grid, TextField } from '@mui/material'
 import { useForm, Controller } from 'react-hook-form'
+import draggableUploader from '../imageUploader/draggableUploader';
 
 const AddProduct = () => {
   const {
@@ -11,6 +12,74 @@ const AddProduct = () => {
   } = useForm()
 
   const handleSubmitForm = (data) => {}
+
+  const [post, setPost] = useState({
+    title: '',
+    desc: '',
+    photos: []
+  });
+
+  const [highlight, setHighlight] = useState(false);
+  const { title, desc, photos } = post;
+
+  const handleFileChange = e =>{
+    let files = e.target.files;
+    console.log(files);
+    handleFiles();
+  }
+
+  const handleFiles = files =>{
+     let photosArr = [];
+    for(let file of files){
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.addEventListener('load', () => {
+        let fileObj = {
+          name: file.name,
+          type: file.type,
+          size: file.size,
+          src: reader.result
+        }
+        photosArr.push(fileObj);
+        setPost({
+          ...post,
+          photos: [...photos,...photosArr]
+        })
+      })
+    }
+  }
+
+  const handleDelete = e =>{
+    let target = e.target.parentElement;
+    let targetindex = target.dataset.imgindex * 1;
+    console.log(target,targetindex);
+    setPost({
+      ...post,
+      photos: [...photos.slice(0,targetindex), ...photos.slice(targetindex + 1)]
+    })
+  }
+
+  const handlehighlight = e =>{
+    e.preventDefault();
+    e.stopPropagation();
+    setHighlight(true);
+  }
+  
+  const handleunhighlight = e =>{
+    e.preventDefault();
+    e.stopPropagation();
+    setHighlight(false);
+  }
+
+  const handledrop = e =>{
+    e.preventDefault();
+    e.stopPropagation();
+    let dt = e.dataTransfer;
+    let files = dt.files;
+    setHighlight(false);
+    handleFiles(files);
+    console.log(files);
+  }
 
   return (
     <div className="m-sm-30">
@@ -111,6 +180,31 @@ const AddProduct = () => {
                 )}
               />
             </Grid>
+
+            <div class="file-upload">
+                <h2>Arrastrar y soltar imagen...</h2>
+                <form class="">
+                    <div class="custom-form-group">
+                        <div className={highlight ? "custom-file-drop-area highlight": "custom-file-drop-area"} onDragEnter={handlehighlight} onDragOver={handlehighlight} onDragLeave={handleunhighlight} onDrop={handledrop}>
+                            <input type="file"name="photos" placeholder="Enter photos" multiple="true" id="filephotos" onChange={handleFileChange}/>
+                            <label for="filephotos">Drag & Drop</label>
+                        </div>
+                        <div class="custom-file-preview">
+                          {
+                            photos.length > 0 && photos.map(
+                              (item, index) =>(
+                                <div class="prev-img" key={index} data-imgindex={index}>
+                                  <span onClick={handleDelete}>&times;</span>
+                                  <img src={item.src} alt={item.name}/>
+                                </div>
+                              )
+                            )
+                          }
+                        </div>
+                    </div>
+                    <button type="submit" class="btn-submit">Submit</button>
+                </form>
+            </div>
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <Button
                 className="w-full"
